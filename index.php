10afe61333700f9876cb85d0e09dfb9329fadc50
Unknown <99053846@mydavinci.nl>
2020-02-11 11:57:10 +0100

    update

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="utf-8">
     <title>Stemwijzer</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="http://stemwijzer.dvc-icta.nl/data.js"></script>
   </head>
   <body>
-    <div class="wrapper">
-      <div class="container">
-        <h1 class="title"></h1>
-        <p>test</p>
+    <div class="w3-container w3-padding-large w3-padding-64 container">
+      <div class="holder">
+        <img class="logo" src="img/logo.svg" alt="">
+        <p class="black bold">Stemwijzer Tweede Kamer 2017</p>
+        <p class="blue bold">Test uw politieke voorkeur aan de hand van 30 stellingen</p>
       </div>
     </div>
   </body>
