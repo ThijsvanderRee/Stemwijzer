6a541cdc8e5b1329a0c7d1cdd638016b5ce8f66a
Unknown <99053846@mydavinci.nl>
2020-02-11 13:02:40 +0100

    "partijen" added and small changes

--- index.php
+++ index.php
@@ -8,11 +8,53 @@
     <script src="http://stemwijzer.dvc-icta.nl/data.js"></script>
   </head>
   <body>
-    <div class="w3-container w3-padding-large w3-padding-64 container">
+    <div class="w3-container w3-padding-64 container">
       <div class="holder">
         <img class="logo" src="img/logo.svg" alt="">
         <p class="black bold">Stemwijzer Tweede Kamer 2017</p>
         <p class="blue bold">Test uw politieke voorkeur aan de hand van 30 stellingen</p>
+        <div class="inner">
+          <div class="left">
+
+          </div>
+          <div class="right">
+            <button href="" class="w3-round start-btn white bold">Start</button>
+          </div>
+        </div>
+      </div>
+      <div class="holder">
+        <p>Of blik terug met de StemmenTracker</p>
+      </div>
+    </div>
+    <div class="w3-container container partijen">
+      <div class="holder">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/vvd.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/pvda.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/pvv.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/sp.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/cda.svg" alt="">
+
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/d66.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/christenunie.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/groenlinks.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/sgp.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/pvdd.svg" alt="">
+
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/50plus.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/ondernemerspartij.png" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/vnl.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/denk.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/nieuwewegen.png" alt="">
+
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/forumvoordemocratie.png" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/burgerbeweging%20(2).png" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/vrijzinnigepartij.png" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/piratenpartij.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/artikel1.png" alt="">
+
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/nietstemmers.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/libertarischepartij.svg" alt="">
+        <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/lokaalindekamer.svg" alt="">
       </div>
     </div>
   </body>
