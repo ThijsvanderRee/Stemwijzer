c1fe13c9aa270dba3a164d05c4c89950d3dc6919
Unknown <99053846@mydavinci.nl>
2020-02-11 14:31:25 +0100

    front page done

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="http://stemwijzer.dvc-icta.nl/data.js"></script>
+    <script src="script.js" charset="utf-8"></script>
   </head>
   <body>
     <div class="w3-container w3-padding-64 container">
@@ -15,18 +16,32 @@
         <p class="blue bold">Test uw politieke voorkeur aan de hand van 30 stellingen</p>
         <div class="inner">
           <div class="left">
-
+            <div class="flex">
+              <img class="icon" src="img/icon-xsable.svg" alt="">
+              <p class="icon_text">Bekijk de toegankelijke versie voor mensen met een visuele beperking</p>
+            </div>
+            <div class="flex">
+              <img class="icon" src="img/icon-lock.svg" alt="">
+              <p class="icon_text">De StemWijzer maakt geen gebruik van cookies. Klik hier voor de volledige privacy statement</p>
+            </div>
           </div>
           <div class="right">
             <button href="" class="w3-round start-btn white bold">Start</button>
           </div>
         </div>
       </div>
+    </div>
+    <div class="w3-container container mt0 dotted_top">
       <div class="holder">
-        <p>Of blik terug met de StemmenTracker</p>
+        <p class="bold">Of blik terug met de StemmenTracker</p>
+        <p>Wat hebben de partijen in het verleden waargemaakt van hun verkiezingsbeloften? 'Track' uw stem met de Stemmentracker, een website die terugblikt op 30 Tweede Kamerstemmingen van de afgelopen regeerperiode. Met welke partij stemde u mee?</p>
       </div>
     </div>
-    <div class="w3-container container partijen">
+    <div class="w3-container container partijen mt0">
+      <div class="holder">
+        <img src="img/1vandaag.svg" class="een_vandaag"alt="">
+        <p class="white bold">Deze partijen doen mee</p>
+      </div>
       <div class="holder">
         <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/vvd.svg" alt="">
         <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/pvda.svg" alt="">
@@ -56,6 +71,19 @@
         <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/libertarischepartij.svg" alt="">
         <img class="partij"src="https://tweedekamer2017.stemwijzer.nl/logos/lokaalindekamer.svg" alt="">
       </div>
+      <div class="holder">
+        <p class="white bold">Deze partijen doen niet mee</p>
+      </div>
+      <div class="holder">
+        <img class="partij" src="https://tweedekamer2017.stemwijzer.nl/public/gfx/parties/geenpeil.png" alt="">
+        <img class="partij" src="https://tweedekamer2017.stemwijzer.nl/public/gfx/parties/jezusleeft.png" alt="">
+        <img class="partij" src="https://tweedekamer2017.stemwijzer.nl/public/gfx/parties/stemnl.png" alt="">
+        <img class="partij" src="https://tweedekamer2017.stemwijzer.nl/public/gfx/parties/mensspirit.png" alt="">
+        <img class="partij" src="https://tweedekamer2017.stemwijzer.nl/public/gfx/parties/vdp.png" alt="">
+      </div>
     </div>
+    <footer>
+      <img class="footer_logo" src="img/logo-footer.png" alt="">
+    </footer>
   </body>
 </html>
