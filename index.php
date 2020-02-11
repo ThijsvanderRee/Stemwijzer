d28064fc1da15d30f8be5b492555d7f4cf87cc64
Unknown <99053846@mydavinci.nl>
2020-02-11 11:15:40 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Stemwijzer</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container">
+        <h1 class="title"></h1>
+        <p>test</p>
+      </div>
+    </div>
+  </body>
+</html>
